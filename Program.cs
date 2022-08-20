7709a3207c83250e6836f00571bca56ac74ff0f8
emrul <emrul.hoque@gmail.com>
2022-08-20 16:35:41 -0500

    added code to create folder

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string myFolderNameWithPath = @"./bin/MyFolder";
+ var dirInfo = System.IO.Directory.CreateDirectory(myFolderNameWithPath);
+Console.WriteLine($"\"{dirInfo.FullName}\" has been created.");
+
+string fileName = System.IO.Path.GetRandomFileName();
+Console.WriteLine($"File \"{fileName}\" will be created in {myFolderNameWithPath}");
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Read & Write File with .NET
+
+
+---
+Links:
+- [How to create a file or folder (C# Programming Guide)](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-create-a-file-or-folder)
+- [How to read from a text file (C# Programming Guide)](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-read-from-a-text-file)
+
